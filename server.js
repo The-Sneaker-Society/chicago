58dafe8b2e0a5459a6fdb7ed46f687ac8d3ccd9d
Alanis Yates <yatesalanis@gmail.com>
2022-05-18 17:26:56 -0400

    inital graphql server

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 import express from "express";
-import morgan from "morgan";
+import { ApolloServer, gql } from "apollo-server-express";
 
-const app = express();
-app.use(morgan('tiny'))
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-const PORT = 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`);
-});
+const resolvers = {
+  Query: {
+    hello() {
+      return "Hello World";
+    },
+  },
+};
+
+async function startServer() {
+  const app = express();
+  const apolloServer = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app: app });
+
+  app.use((req, res) => {
+    res.send("Hello from express apollo server");
+  });
+
+
+    app.listen(4000, () => {
+      console.log(`Server is running on port ${4000}...`);
+    })
+ 
+}
+
+startServer();
