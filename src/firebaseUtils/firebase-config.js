02b474b8fccc9c8ce018c0da64af529124edb987
Alanis Yates <yatesalanis@gmail.com>
2023-03-26 18:08:12 -0400

    more auth cleanup

--- src/firebaseUtils/firebase-config.js
+++ src/firebaseUtils/firebase-config.js
@@ -1,12 +1,14 @@
-const admin = require("firebase-admin");
+import admin from "firebase-admin";
+
+const serviceAccount = {
+  projectId: process.env.FB_PROJECT_ID,
+  clientEmail: process.env.FB_CLIENT_EMAIL,
+  privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FB_PROJECT_ID,
-    clientEmail: process.env.FB_CLIENT_EMAIL,
-    privateKey: process.env.FB_PRIVATE_KEY,
-  }),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FB_DB_URL,
 });
 
-module.exports = admin;
+export default admin;
