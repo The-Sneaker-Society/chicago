cc1f54ae2a62793b2ae35166af15dad669662d5b
AlanisYates <yatesalanis@gmail.com>
2023-06-18 23:48:29 -0400

    first pass

--- src/cron-jobs/cronLogger.js
+++ src/cron-jobs/cronLogger.js
@@ -1,13 +1,37 @@
 import cron from 'node-cron';
 import { sendEmail } from '../utils/sendEmail';
+import EmailModel from '../models/Email.model';
 
-const emailCron = cron.schedule('0 6 * * *', () => {
+const emailSender = async () => {
+  try {
+    const emails = await EmailModel.find();
+    console.log(emails);
+
+    emails.forEach((emailObj) => {
+      const { email, firstName, lastName, recieved } = emailObj;
+      console.log({
+        email: email,
+        name: `${firstName} ${lastName}`,
+        recieved: recieved,
+      });
+
+      // Perform the necessary actions for sending the email and updating the received emails
+      // sendEmail(email, subject, text, html)
+      // updateReceivedEmails(emailObj._id);
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const emailCron = cron.schedule('*/10 * * * * *', async () => {
+  await emailSender();
   // Sending the email
-  sendEmail(
-    'Thank You',
-    'alanisyates96@gmail.com',
-    { time: new Date().toLocaleString() },
-    'src/emails/test.html'
-  );
+  //   sendEmail(
+  //     'Thank You',
+  //     'alanisyates96@gmail.com',
+  //     { time: new Date().toLocaleString() },
+  //     'src/emails/test.html'
+  //   );
 });
 export default emailCron;
