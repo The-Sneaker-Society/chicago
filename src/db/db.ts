c7984f20abe1cef302fe1a68e2bad992ee1ba0e6
AlanisYates <yatesalanis@gmail.com>
2024-01-27 17:45:14 -0800

    add husky

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,7 +2,7 @@ import mongoose, { Connection } from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: 'config.env' });
-console.log('hello')
+
 const DATABASE_URL = process.env.ATLAS_URI;
 
 const connectDb = (): Promise<Connection> => {
