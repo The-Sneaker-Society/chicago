c3ad00fd8f356dbf732ba93bf2dd676d5eaef274
AlanisYates <yatesalanis@gmail.com>
2024-01-27 16:49:20 -0800

    working ts applo server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello ts App')
+import express, { Application } from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, world!',
+  },
+};
+
+const startServer = async () => {
+  const app: Application = express();
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  await server.start();
+
+  server.applyMiddleware({ app } as any);
+
+  const PORT = process.env.PORT || 4000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+};
+
+startServer();
