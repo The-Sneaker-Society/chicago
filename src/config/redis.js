ac790df361231173b04ac64a15b6c9e997457aae
Alanis Yates <yatesalanis@gmail.com>
2026-04-11 20:23:10 -0500

    fix duplicate error

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,7 +6,21 @@ dotenv.config({ path: "config.env" });
 const redisUrl = process.env.REDIS_URL;
 
 const redisConfig = redisUrl
-  ? redisUrl
+  ? { 
+      url: redisUrl,
+      maxRetriesPerRequest: 1,
+      retryStrategy(times) {
+        if (times > 3) return null;
+        return Math.min(times * 200, 2000);
+      },
+      reconnectOnError(err) {
+        const targetErrors = ["READONLY", "ECONNRESET", "ETIMEDOUT"];
+        if (targetErrors.some(e => err.message.includes(e))) {
+          return true;
+        }
+        return false;
+      }
+    }
   : {
       host: process.env.REDIS_HOST || "127.0.0.1",
       port: parseInt(process.env.REDIS_PORT || "6379"),
